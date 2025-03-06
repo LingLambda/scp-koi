87b3bba1f8d472eeff9c82feff282dffb00708cf
LingLambda <abc1514671906@163.com>
2025-03-06 18:31:46 +0800

    init

---
+++ src/Service/PuppUtil.ts
@@ -0,0 +1,54 @@
+import { Context, h } from "koishi";
+import "koishi-plugin-puppeteer";
+import { Config} from "..";
+import { Browser, Page } from "puppeteer-core";
+import Puppeteer from "koishi-plugin-puppeteer";
+
+const MAIN_URL = "https:///scp-wiki.wikidot.com";
+const CN_URL = "https:///scp-wiki-cn.wikidot.com";
+let baseUrl: string;
+let puppeteer: Puppeteer;
+let browser: Browser;
+let page: Page;
+
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function init(ctx: Context, config: Config) {
+  browser = ctx.puppeteer.browser;
+  puppeteer = ctx.puppeteer;
+  baseUrl = config.CNURL ? CN_URL : MAIN_URL;
+}
+
+export async function getSCPInfo(
+  options: { name?: string; number?: string } = {}
+) {
+  if (!browser) {
+    await puppeteer.start();
+  }
+  page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+  );
+  const { name, number } = options;
+  let param = "";
+  if (name) {
+    param = name;
+  } else if (number) {
+    param = `scp-${number.padStart(3, "0")}`;
+  }
+
+  await page.goto(`${baseUrl}/${param}`, {
+    waitUntil: "networkidle2", // 等待加载完毕
+  });
+  const mainElement = await page.waitForSelector("#main-content");
+  const screenshotData = await mainElement.screenshot();
+
+  await page.close();
+  return h.image(screenshotData, "image/png");
+}
+
+export async function screenshotFull() {
+  return await page.screenshot({ fullPage: true });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
+import { getSCPInfo, init } from "./Service/PuppUtil";
 
-export const name = 'scp-564744'
+export const name = "scp-koi";
 
-export interface Config {}
+export const inject = ["puppeteer"];
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  CNURL: boolean;
+  // imageMode?: boolean;
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    CNURL: Schema.boolean()
+      .default(true)
+      .description("访问国区wiki，关闭后使用主站"),
+    // imageMode: Schema.boolean().default(true).description("图片模式"),
+  }),
+]);
+
+export function apply(ctx: Context, config: Config) {
+  init(ctx, config);
+  ctx
+    .command("SCP <number>", "查询对应编号SCP")
+    .action(async ({ session }, number) => {
+      await getInfo(session, { number: number });
+    })
+    .example("SCP 096 查询scp-096");
+  ctx
+    .command("SCPP <name>", "访问SCP wiki任意页面")
+    .action(async ({ session }, name) => {
+      await getInfo(session, { name: name });
+    })
+    .example("SCPP user-curated-lists 访问用户推荐清单页");
+}
+
+async function getInfo(
+  session,
+  options: { name?: string; number?: string } = {}
+) {
+  session.send("███████（数据删除）");
+  const image = await getSCPInfo(options);
+  session.send(image);
+  return;
 }
